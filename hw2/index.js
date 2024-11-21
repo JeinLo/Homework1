c04cb2d40d1749e245cb08e2707ef5123030a1d3
JeinLo <baranova110594@gmail.com>
2024-11-21 20:01:32 +0300

    Add files via upload

---
+++ hw2/index.js
@@ -0,0 +1,67 @@
+//Задание 1
+
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2
+
+const dateiPhone = 2020;
+alert(dateiPhone);
+
+//Задание 3
+
+const nameCreator = 'Брендан Эйх';
+alert(nameCreator);
+
+//Задание 4
+
+let b = 10;
+let c = 2;
+alert(b+c);
+alert(b-c);
+alert(b*c);
+alert(b/c);
+
+//Задание 5
+
+let result = 2**5;
+alert(result);
+
+//Задание 6 
+
+let z = 9;
+let x = 2;
+let res = z%x;
+alert(res);
+
+//Задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//Задание 8
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+//Задание 9
+
+const user = {
+    name: 'Петр', 
+    age: 20, 
+    isAdmin: true,
+
+}
+
+//Задание 10
+
+let userName = prompt('Твое имя');
+alert(`Привет ${userName}!`);
